1bd12a43b01787bcd10cf18ab6fe2f633d1bfa3a
designerofthing <designerofthings@gmail.com>
2020-09-17 10:42:34 +0200

    adds pagination tests

--- cypress/integration/visitorCanViewListofCharacters.feature.js
+++ cypress/integration/visitorCanViewListofCharacters.feature.js
@@ -17,7 +17,10 @@ describe("Visitor can view a list of characters", () => {
     cy.get("#header").find("img#logo").should("be.visible");
   });
   it("showns description text inside the header", () => {
-    cy.get("#header").should("contain", "View and search for your favourite Star Wars characters");
+    cy.get("#header").should(
+      "contain",
+      "View and search for your favourite Star Wars characters"
+    );
   });
 
   it("shows a list of characters", () => {
@@ -25,5 +28,15 @@ describe("Visitor can view a list of characters", () => {
       cy.get("#character-list").should("contain", "Luke Skywalker");
       cy.get("#character-list").should("contain", "Obi-Wan Kenobi");
     });
+  });
+
+  it("shows a next page button", () => {
+    cy.get("#next-button").should("be.visible");
+  });
+  it("shows the next page list of character on button click", () => {
+    cy.get("#next-button").click();
+    cy.get("#character-list").should("contain", "Anakin Skywalker");
+    cy.get("#character-list").should("contain", "Palpatine");
+    cy.get("#previous-button").should("be.visible");
   });
 });
