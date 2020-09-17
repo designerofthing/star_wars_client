2adda80dd7a929548ccb2d02e01ead4f5d43c528
designerofthing <designerofthings@gmail.com>
2020-09-17 10:12:50 +0200

    installs axios, adds get request, hook and mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-     Star Wars App
+const App = () => {
+  const [names, setNames] = useState([]);
+
+  const getNames = async () => {
+    try {
+      const response = await axios.get("https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people");
+      setNames(response.data.results)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getNames()
+  }, [])
+
+  let characterNames = names.map((name) => {
+    return(
+      <div>
+        <p>{name.name}</p>
+      </div>
+    )
+
+  })
+
+  return <div>
+    <header id="header">
+
+    </header>
+    <div id="main-container">
+    <div id="character-list">
+      {characterNames}
     </div>
-  );
-}
+    </div>
+    <footer id="footer">
+      
+    </footer>
+    </div>;
+};
 
 export default App;
